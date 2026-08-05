9056da1883d0278e3d162c0d22c85849038078ee
DestriaAngelyne <angelynedestria@gmail.com>
2026-08-05 11:12:02 +0700

    fix(client-logos2): tambah fallback badge di Row 2 untuk logo yang belum ada gambarnya

--- resources/js/components/LandingClientLogos2.vue
+++ resources/js/components/LandingClientLogos2.vue
@@ -54,13 +54,15 @@
         <div class="cls-track reverse" :class="{ paused: paused2 }" :style="{ '--dur': dur2 + 's' }">
           <div v-for="(logo, i) in row2" :key="'c' + i" class="cls-item">
             <div class="cls-card">
-              <img :src="logo.url" :alt="logo.name" class="cls-img" draggable="false"/>
+              <img v-if="logo.url" :src="logo.url" :alt="logo.name" class="cls-img" draggable="false"/>
+              <span v-else class="cls-badge">{{ logo.short || logo.name }}</span>
               <div class="cls-card-name">{{ logo.name }}</div>
             </div>
           </div>
           <div v-for="(logo, i) in row2" :key="'d' + i" class="cls-item" aria-hidden="true">
             <div class="cls-card">
-              <img :src="logo.url" :alt="logo.name" class="cls-img" draggable="false"/>
+              <img v-if="logo.url" :src="logo.url" :alt="logo.name" class="cls-img" draggable="false"/>
+              <span v-else class="cls-badge">{{ logo.short || logo.name }}</span>
               <div class="cls-card-name">{{ logo.name }}</div>
             </div>
           </div>
